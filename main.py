a73dc6b4f2e195f84bb1489255eeced860ab8311
scycer <scycer1@hotmail.com>
2024-05-22 17:33:17 +1000

    working API

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from src.utils import getName
+from flask import Flask
 
-print("Hello!")
-name = getName()
-print("Hello, " + name + "!")
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
